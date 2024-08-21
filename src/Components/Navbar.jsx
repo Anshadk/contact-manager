1fad96e6651fc7283eef249ea459b5dbb304919e
Anshadk <anshadkammaje786@gmail.com>
2024-08-21 12:32:12 +0530

    navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar({hide}) {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">Contacts</Link>
+          <Link className="navbar-brand" to="/">{hide? "Contacts":"Home"}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
